904ecf2c508770d454075b9ffa35e9729199603c
Plimon Howard V <plimon.howard@gmail.com>
2020-09-15 01:29:02 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,15 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arry)
+  indx = 0
+  while (indx < arry.length)
+    yield (arry[indx])
+      indx += 1
+    end
+    arry
 end
+
+collection = [1, 2, 3, 4]
+newArry = []
+my_each(collection) do |i|
+  newArry << i
+end
+newArry
