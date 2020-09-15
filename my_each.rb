6829b540d8e1f5e653fde89c855f7991dd110f97
Plimon Howard V <plimon.howard@gmail.com>
2020-09-15 01:21:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arry) 
+  if block_given?
+    indx = 0
+
+    while (indx < arry.length)
+      yield (arry[i])
+      indx += 1
+    end
+    arry
+  else
+    puts "No block given"
+  end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
